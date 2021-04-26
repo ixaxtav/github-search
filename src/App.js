e47b3bf45a4be21476e8408b63db1bde86c3280f
ixaxtav <ixaxtav@gmail.com>
2021-04-26 08:32:07 -0400

    navbar ccomponent created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {NavbarGithub} from './components/navbar'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <NavbarGithub title="Ixax Github Search"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
